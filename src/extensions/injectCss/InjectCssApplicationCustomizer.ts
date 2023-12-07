3d43fbd520008e330f78803469d1ab4d5d50f283
Andy Prosser <andy@theexectechs.com>
2023-12-08 10:58:06 +1100

    Update All Dependencies FIX: Build Issues

--- src/extensions/injectCss/InjectCssApplicationCustomizer.ts
+++ src/extensions/injectCss/InjectCssApplicationCustomizer.ts
@@ -1,7 +1,7 @@
 import { override } from '@microsoft/decorators';
 import { Log } from '@microsoft/sp-core-library';
 import {
-  BaseApplicationCustomizer
+    BaseApplicationCustomizer
 } from '@microsoft/sp-application-base';
 
 import * as strings from 'InjectCssApplicationCustomizerStrings';
@@ -10,27 +10,27 @@ const LOG_SOURCE: string = 'InjectCssApplicationCustomizer';
 
 
 export interface IInjectCssApplicationCustomizerProperties {
-  cssurl: string;
+    cssurl: string;
 }
 
 export default class InjectCssApplicationCustomizer
-  extends BaseApplicationCustomizer<IInjectCssApplicationCustomizerProperties> {
+    extends BaseApplicationCustomizer<IInjectCssApplicationCustomizerProperties> {
 
-  @override
-  public onInit(): Promise<void> {
-    Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
+    @override
+    public onInit(): Promise<void> {
+        Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
 
-    const cssUrl: string = this.properties.cssurl;
-    if (cssUrl) {
-        // inject the style sheet
-        const head: any = document.getElementsByTagName("head")[0] || document.documentElement;
-        let customStyle: HTMLLinkElement = document.createElement("link");
-        customStyle.href = cssUrl;
-        customStyle.rel = "stylesheet";
-        customStyle.type = "text/css";
-        head.insertAdjacentElement("beforeEnd", customStyle);
+        const cssUrl: string = this.properties.cssurl;
+        if (cssUrl) {
+            // inject the style sheet
+            const head: any = document.getElementsByTagName("head")[0] || document.documentElement;
+            let customStyle: HTMLLinkElement = document.createElement("link");
+            customStyle.href = cssUrl;
+            customStyle.rel = "stylesheet";
+            customStyle.type = "text/css";
+            head.insertAdjacentElement("beforeEnd", customStyle);
+        }
+
+        return Promise.resolve();
     }
-
-    return Promise.resolve();
-  }
 }
